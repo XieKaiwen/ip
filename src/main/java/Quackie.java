d1c873d1b12f48949cd3f30cef927721b423c7d9
Xie Kaiwen <xiekaiwen3@gmail.com>
2026-08-20 16:18:49 +0800

    edit name of duke

---
+++ src/main/java/Quackie.java
@@ -0,0 +1,14 @@
+/**
+ * Entry point for the Quackie chatbot.
+ */
+public class Quackie {
+    public static void main(String[] args) {
+        String banner = "                           _      _      \n"
+                + "  __ _  _   _   __ _  ___| | __ (_)  ___ \n"
+                + " / _` || | | | / _` |/ __| |/ / | | / _ \\ \n"
+                + "| (_| || |_| || (_| | (__|   <  | ||  __/\n"
+                + " \\__, | \\__,_| \\__,_|\\___|_|\\_\\ |_|\\___|\n"
+                + "    |_|                                  \n";
+        System.out.println(banner);
+    }
+}
